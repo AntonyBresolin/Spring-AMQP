01dbc28de569777ab6cacab0f35d948dc0a7c63c
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-12-07 16:03:01 -0300

    feat: initial structure

---
+++ cashback-service/src/test/java/github/antonybresolin/cashback_service/CashbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package github.antonybresolin.cashback_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
